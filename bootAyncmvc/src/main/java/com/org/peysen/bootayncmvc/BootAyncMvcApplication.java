eeb4d69e9e405f386a99d67294f6ca291e457430
black peysen <peysen@gmail.com>
2019-08-27 07:44:22 +0800

    0827：异步Servlet

---
+++ bootAyncmvc/src/main/java/com/org/peysen/bootayncmvc/BootAyncMvcApplication.java
@@ -0,0 +1,13 @@
+package com.org.peysen.bootayncmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAyncMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAyncMvcApplication.class, args);
+    }
+
+}
