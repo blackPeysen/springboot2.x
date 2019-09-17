e13812f764c4ec3de25c3014ef7fbdcafdfec5c2
blackPeysen <peysen20@gmail.com>
2019-09-17 15:12:30 +0800

    0917：新增shiro模块

---
+++ boot-shiro/src/main/java/com/org/peysen/bootshiro/config/CustomRealm.java
@@ -0,0 +1,90 @@
+package com.org.peysen.bootshiro.config;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: java类作用描述
+ * Created by mengmeng.Pei
+ * 2019/9/16 16:59
+ */
+public class CustomRealm extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username = (String) principalCollection.getPrimaryPrincipal();
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+
+        //从数据库或者缓存中获取角色数据
+        Set<String> roles = getRolesByUsername(username);
+        //从数据库或者缓存中获取权限数据
+        Set<String> permissions = getPermissions(username);
+
+        info.setRoles(roles);
+        info.setStringPermissions(permissions);
+        return info;
+    }
+
+    private Set<String> getPermissions(String userName){
+        Set<String> sets = new HashSet<>();
+        sets.add("user:delete");
+        sets.add("user:update");
+        return sets;
+    }
+
+    private Set<String> getRolesByUsername(String userName){
+        System.out.println("模拟从数据库中获取角色数据。。。");
+        Set<String> sets = new HashSet<>();
+        sets.add("admin");
+        sets.add("user");
+        return sets;
+    }
+
+
+
+    /**
+     * 这里可以注入userService,为了方便演示，我就写死了帐号了密码
+     * private UserService userService;
+     * <p>
+     * 获取即将需要认证的信息
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("-------身份认证方法--------");
+        //从主体传过来的认证信息中获取用户名
+        String userName = (String) authenticationToken.getPrincipal();
+
+        //从主体传过来的认证信息中获取认证密码
+        String userPwd = new String((char[]) authenticationToken.getCredentials());
+
+        //根据用户名从数据库获取密码
+        String password = "2415b95d3203ac901e287b76fcef640b";
+
+        if (userName == null) {
+            throw new AccountException("用户名不正确");
+        } else if (!userPwd.equals(userPwd)) {
+            throw new AccountException("密码不正确");
+        }
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配
+        return new SimpleAuthenticationInfo(userName, password,
+                ByteSource.Util.bytes(userName + "salt"), getName());
+    }
+
+    public static String MD5Pwd(String username, String pwd) {
+        // 加密算法MD5
+        // salt盐 username + salt
+        // 迭代次数
+        String md5Pwd = new SimpleHash("MD5", pwd,
+                ByteSource.Util.bytes(username + "salt"), 2).toHex();
+        return md5Pwd;
+    }
+}
---
+++ boot-shiro/src/main/java/com/org/peysen/bootshiro/config/ShiroConfig.java
@@ -0,0 +1,189 @@
+package com.org.peysen.bootshiro.config;
+
+import com.org.peysen.bootshiro.cache.RedisCache;
+import com.org.peysen.bootshiro.cache.RedisCacheManager;
+import com.org.peysen.bootshiro.filter.RolesOrFilter;
+import com.org.peysen.bootshiro.session.CustomSessionManager;
+import com.org.peysen.bootshiro.session.RedisSessionDao;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.DefaultSessionManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @Description: java类作用描述
+ * Created by mengmeng.Pei
+ * 2019/9/16 16:58
+ */
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager defaultSecurityManager = new DefaultWebSecurityManager();
+
+        //设置realm
+        defaultSecurityManager.setRealm(customRealm());
+        //设置sessionManager管理器
+        defaultSecurityManager.setSessionManager(sessionManager());
+        //设置cacheManager缓存管理器
+        defaultSecurityManager.setCacheManager(cacheManager());
+        //设置rememberMeManager缓存管理器
+        defaultSecurityManager.setRememberMeManager(cookieRememberMeManager());
+
+        return defaultSecurityManager;
+    }
+
+    //配置过滤器
+    @Bean
+    public RolesOrFilter rolesOrFilter(){
+        return new RolesOrFilter();
+    }
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/notRole");
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/", "anon");
+        filterChainDefinitionMap.put("/front/**", "anon");
+        filterChainDefinitionMap.put("/api/**", "anon");
+
+        filterChainDefinitionMap.put("/admin/**", "authc");
+        filterChainDefinitionMap.put("/user/**", "authc");
+
+        //测试角色
+        filterChainDefinitionMap.put("/testRole", "roles[admin]");
+        filterChainDefinitionMap.put("/testRole1", "rolesOrFilter[admin,admin1]");
+
+        //测试角色
+        filterChainDefinitionMap.put("/testPerm", "perms[user:delete]");
+        filterChainDefinitionMap.put("/testPerm", "perms[user:delete,user:update]");
+
+        //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截 剩余的都需要认证
+        filterChainDefinitionMap.put("/**", "authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        //设置过滤器
+        Map<String,Filter> mapFilter = new HashedMap();
+        mapFilter.put("rolesOrFilter", rolesOrFilter());
+        shiroFilterFactoryBean.setFilters(mapFilter);
+
+        return shiroFilterFactoryBean;
+
+    }
+
+    @Bean
+    public DefaultWebSessionManager sessionManager(){
+        //设置自定义sessionManager管理器：CustomSessionManager
+        DefaultWebSessionManager sessionManager = new CustomSessionManager();
+        //设置自定义SessionDAO：redisSessionDao
+        sessionManager.setSessionDAO(redisSessionDao());
+        return sessionManager;
+    }
+
+    @Bean
+    public SessionDAO redisSessionDao(){
+        return new RedisSessionDao();
+    }
+
+    @Bean
+    public CacheManager cacheManager(){
+        return new RedisCacheManager();
+    }
+
+    @Bean
+    public RedisCache redisCache(){
+        return new RedisCache<Object,Object>();
+    }
+
+    @Bean
+    public CustomRealm customRealm() {
+        CustomRealm customRealm = new CustomRealm();
+        // 告诉realm,使用credentialsMatcher加密算法类来验证密文
+        customRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        //不使用缓存数据
+        customRealm.setCachingEnabled(false);
+        return customRealm;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    /**
+     * *
+     * 开启Shiro的注解(如@RequiresRoles,@RequiresPermissions),需借助SpringAOP扫描使用Shiro注解的类,并在必要时进行安全逻辑验证
+     * *
+     * 配置以下两个bean(DefaultAdvisorAutoProxyCreator(可选)和AuthorizationAttributeSourceAdvisor)即可实现此功能
+     * * @return
+     */
+    @Bean
+    @DependsOn({"lifecycleBeanPostProcessor"})
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor() {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager());
+        return authorizationAttributeSourceAdvisor;
+    }
+
+    @Bean
+    public CookieRememberMeManager cookieRememberMeManager(){
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(simpleCookie());
+
+        return cookieRememberMeManager;
+    }
+
+    @Bean
+    public SimpleCookie simpleCookie(){
+        SimpleCookie simpleCookie = new SimpleCookie();
+        simpleCookie.setMaxAge(200000);
+
+        return simpleCookie;
+    }
+
+    @Bean(name = "credentialsMatcher")
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        // 散列算法:这里使用MD5算法;
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");
+        // 散列的次数，比如散列两次，相当于 md5(md5(""));
+        hashedCredentialsMatcher.setHashIterations(2);
+        // storedCredentialsHexEncoded默认是true，此时用的是密码加密用的是Hex编码；false时用Base64编码
+        hashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+        return hashedCredentialsMatcher;
+    }
+
+}
