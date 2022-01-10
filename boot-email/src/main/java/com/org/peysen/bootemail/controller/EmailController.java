2d937e77ea824a096f3fb2115283806887127024
blackPeysen <peysen20@gmail.com>
2022-01-10 17:41:49 +0800

    0110: 新增zk工具类

--- boot-email/src/main/java/com/org/peysen/bootemail/controller/EmailController.java
+++ boot-email/src/main/java/com/org/peysen/bootemail/controller/EmailController.java
@@ -1,6 +1,5 @@
 package com.org.peysen.bootemail.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- boot-zk/src/main/java/com/org/peysen/bootzk/configuration/ZookeeperConfig.java
+++ boot-zk/src/main/java/com/org/peysen/bootzk/configuration/ZookeeperConfig.java
@@ -23,27 +23,27 @@ public class ZookeeperConfig {
 
     @Bean(name = "zkClient")
     public ZooKeeper zkClient(){
-        ZooKeeper zooKeeper=null;
+        ZooKeeper zooKeeper = null;
         try {
             final CountDownLatch countDownLatch = new CountDownLatch(1);
-            //连接成功后，会回调watcher监听，此连接操作是异步的，执行完new语句后，直接调用后续代码
-            //  可指定多台服务地址 127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183
-             zooKeeper = new ZooKeeper(connectString, timeout, new Watcher() {
+            // 连接成功后，会回调watcher监听，此连接操作是异步的，执行完new语句后，直接调用后续代码
+            // 可指定多台服务地址 127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183
+            zooKeeper = new ZooKeeper(connectString, timeout, new Watcher() {
                 @Override
                 public void process(WatchedEvent event) {
-                    if(Watcher.Event.KeeperState.SyncConnected==event.getState()){
+                    if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
                         //如果收到了服务端的响应事件,连接成功
                         countDownLatch.countDown();
                     }
                 }
             });
             countDownLatch.await();
-            log.info("【初始化ZooKeeper连接状态....】={}",zooKeeper.getState());
-
-        }catch (Exception e){
-            log.error("初始化ZooKeeper连接异常....】={}",e);
+            log.info("【初始化ZooKeeper连接状态....】= {}", zooKeeper.getState());
+        } catch (Exception e) {
+            log.error("初始化ZooKeeper连接异常....】={}", e);
         }
-         return  zooKeeper;
+
+        return zooKeeper;
     }
 
 
